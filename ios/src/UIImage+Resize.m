e65fba4995f8b13ce2f58f0056674afb4389c3d0
Nver Abgaryan <abgaryannwer@gmail.com>
2022-09-01 11:31:16 +0400

    Hide angle view

--- ios/src/UIImage+Resize.m
+++ ios/src/UIImage+Resize.m
@@ -22,57 +22,57 @@
     
 	CGFloat scaleRatio = dstSize.width / srcSize.width;
 	UIImageOrientation orient = self.imageOrientation;
-//	CGAffineTransform transform = CGAffineTransformIdentity;
-//	switch(orient) {
-//
-//		case UIImageOrientationUp: //EXIF = 1
-//			transform = CGAffineTransformIdentity;
-//			break;
-//
-//		case UIImageOrientationUpMirrored: //EXIF = 2
-//			transform = CGAffineTransformMakeTranslation(srcSize.width, 0.0);
-//			transform = CGAffineTransformScale(transform, -1.0, 1.0);
-//			break;
-//
-//		case UIImageOrientationDown: //EXIF = 3
-//			transform = CGAffineTransformMakeTranslation(srcSize.width, srcSize.height);
-//			transform = CGAffineTransformRotate(transform, M_PI);
-//			break;
-//
-//		case UIImageOrientationDownMirrored: //EXIF = 4
-//			transform = CGAffineTransformMakeTranslation(0.0, srcSize.height);
-//			transform = CGAffineTransformScale(transform, 1.0, -1.0);
-//			break;
-//
-//		case UIImageOrientationLeftMirrored: //EXIF = 5
-//			dstSize = CGSizeMake(dstSize.height, dstSize.width);
-//			transform = CGAffineTransformMakeTranslation(srcSize.height, srcSize.width);
-//			transform = CGAffineTransformScale(transform, -1.0, 1.0);
-//			transform = CGAffineTransformRotate(transform, 3.0 * M_PI_2);
-//			break;
-//
-//		case UIImageOrientationLeft: //EXIF = 6
-//			dstSize = CGSizeMake(dstSize.height, dstSize.width);
-//			transform = CGAffineTransformMakeTranslation(0.0, srcSize.width);
-//			transform = CGAffineTransformRotate(transform, 3.0 * M_PI_2);
-//			break;
-//
-//		case UIImageOrientationRightMirrored: //EXIF = 7
-//			dstSize = CGSizeMake(dstSize.height, dstSize.width);
-//			transform = CGAffineTransformMakeScale(-1.0, 1.0);
-//			transform = CGAffineTransformRotate(transform, M_PI_2);
-//			break;
-//
-//		case UIImageOrientationRight: //EXIF = 8
-//			dstSize = CGSizeMake(dstSize.height, dstSize.width);
-//			transform = CGAffineTransformMakeTranslation(srcSize.height, 0.0);
-//			transform = CGAffineTransformRotate(transform, M_PI_2);
-//			break;
-//
-//		default:
-//			[NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
-//
-//	}
+	CGAffineTransform transform = CGAffineTransformIdentity;
+	switch(orient) {
+
+		case UIImageOrientationUp: //EXIF = 1
+			transform = CGAffineTransformIdentity;
+			break;
+
+		case UIImageOrientationUpMirrored: //EXIF = 2
+			transform = CGAffineTransformMakeTranslation(srcSize.width, 0.0);
+			transform = CGAffineTransformScale(transform, -1.0, 1.0);
+			break;
+
+		case UIImageOrientationDown: //EXIF = 3
+			transform = CGAffineTransformMakeTranslation(srcSize.width, srcSize.height);
+			transform = CGAffineTransformRotate(transform, M_PI);
+			break;
+
+		case UIImageOrientationDownMirrored: //EXIF = 4
+			transform = CGAffineTransformMakeTranslation(0.0, srcSize.height);
+			transform = CGAffineTransformScale(transform, 1.0, -1.0);
+			break;
+
+		case UIImageOrientationLeftMirrored: //EXIF = 5
+			dstSize = CGSizeMake(dstSize.height, dstSize.width);
+			transform = CGAffineTransformMakeTranslation(srcSize.height, srcSize.width);
+			transform = CGAffineTransformScale(transform, -1.0, 1.0);
+			transform = CGAffineTransformRotate(transform, 3.0 * M_PI_2);
+			break;
+
+		case UIImageOrientationLeft: //EXIF = 6
+			dstSize = CGSizeMake(dstSize.height, dstSize.width);
+			transform = CGAffineTransformMakeTranslation(0.0, srcSize.width);
+			transform = CGAffineTransformRotate(transform, 3.0 * M_PI_2);
+			break;
+
+		case UIImageOrientationRightMirrored: //EXIF = 7
+			dstSize = CGSizeMake(dstSize.height, dstSize.width);
+			transform = CGAffineTransformMakeScale(-1.0, 1.0);
+			transform = CGAffineTransformRotate(transform, M_PI_2);
+			break;
+
+		case UIImageOrientationRight: //EXIF = 8
+			dstSize = CGSizeMake(dstSize.height, dstSize.width);
+			transform = CGAffineTransformMakeTranslation(srcSize.height, 0.0);
+			transform = CGAffineTransformRotate(transform, M_PI_2);
+			break;
+
+		default:
+			[NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
+
+	}
 	
 	/////////////////////////////////////////////////////////////////////////////
 	// The actual resize: draw the image on a new context, applying a transform matrix
